0cb634f86924e39f1ff2839bb87aab671415b273
Souptik09 <75028313+Souptik09@users.noreply.github.com>
2021-03-22 00:45:28 +0530

    Create tinary operator.c

---
+++ March/tinary operator.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+main() 
+  
+{  
+    int n1,n2,n3,max;  
+     printf("Please Enter Three different values\n");
+     scanf("%d%d%d%d",&n1,&n2,&n3,&max);
+     max = (n1>n2 && n1>n3)?n1:(n2>n3)?n2:n3;
+  
+     //[//max =(n1 > n2) ? (n1 > n3 ? n1 : n3) : (n2 > n3  ? n2 : n3);] 
+  
+    printf("largest number among %d,%d,%d is %d\n",n1,n2,n3,max);
+ 
+}
